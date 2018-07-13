ca308e686ee110576756f79f0c5ee6e035791de1
Jean-Martin Vlaeminck <jean-martin.vlaeminck@student.uclouvain.be>
2018-07-13 15:04:08 +0200

    Replacing getpagesize() by getconf(_SC_PAGESIZE), which is recommanded by POSIX standard

--- student/CTester/trap.c
+++ student/CTester/trap.c
@@ -6,19 +6,20 @@
 
 void *trap_buffer(size_t size, int type, int flags, void *data)
 {
-    int nb_pages = (size / getpagesize()) + 2;
-    void *ptr = mmap(NULL, getpagesize() * nb_pages, PROT_READ | PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    size_t pagesize = sysconf(_SC_PAGESIZE);
+    int nb_pages = (size / pagesize) + 2;
+    void *ptr = mmap(NULL, pagesize * nb_pages, PROT_READ | PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
     if (ptr == MAP_FAILED) {
         return NULL;
     }
 
     void *buf_start;
     if (type == TRAP_LEFT) {
-        buf_start = ptr + getpagesize();
-        mprotect(ptr, getpagesize(), PROT_NONE);
+        buf_start = ptr + pagesize;
+        mprotect(ptr, pagesize, PROT_NONE);
     } else if (type == TRAP_RIGHT) {
-        buf_start = ptr + (nb_pages - 1) * getpagesize() - size;
-        mprotect(ptr + (nb_pages - 1)*getpagesize(), getpagesize(), PROT_NONE);
+        buf_start = ptr + (nb_pages - 1) * pagesize - size;
+        mprotect(ptr + (nb_pages - 1)*pagesize, pagesize, PROT_NONE);
     } else {
         return NULL;
     }
