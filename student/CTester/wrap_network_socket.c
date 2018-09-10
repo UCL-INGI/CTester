3ec71e59535e307e4712a6b91137bff04a705c21
Jean-Martin Vlaeminck <jean-martin.vlaeminck@student.uclouvain.be>
2018-09-10 14:59:08 +0200

    Add delayed chunks to recv
    Not well-tested, however

--- student/CTester/wrap_network_socket.c
+++ student/CTester/wrap_network_socket.c
@@ -22,6 +22,7 @@
 #include "wrap.h"
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define BILLION (1000*1000*1000)
 
 
 int     __real_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
@@ -44,6 +45,268 @@ extern bool wrap_monitoring;
 extern struct wrap_stats_t stats;
 extern struct wrap_monitor_t monitored;
 extern struct wrap_fail_t failures;
+
+
+/**
+ * Auxiliary structures and functions
+ */
+
+void getnanotime(struct timespec *res)
+{
+    clock_gettime(CLOCK_REALTIME, res);
+}
+
+int64_t get_time_interval(const struct timespec *pasttime, const struct timespec *curtime)
+{
+    int64_t past = pasttime->tv_sec;
+    past *= 1000*1000*1000;
+    past += pasttime->tv_nsec;
+    int64_t cur = curtime->tv_sec;
+    cur *= 1000*1000*1000;
+    cur += curtime->tv_nsec;
+    int64_t interval = cur - past;
+    if (interval < 0) {
+        fprintf(stderr, "BUG: negative time interval");
+    }
+    return interval;
+}
+
+
+/**
+ * recv-related
+ */
+
+// #define RECV_MODE_BUFCHUK 1 // not used
+
+/**
+ * Precision for the 'interval' field.
+ * It contains the wait time for the current chunk, as measured from the previous call.
+ * It may be positive: in this case, it means the next data was not available
+ * at the end of the previous call, and it is the time the current call has
+ * to wait before accessing data. Minus the interval between the two calls.
+ * It may be negative: in this case, it represents the opposite of the amout
+ * of time the current chunk has been available.
+ * Thanks to this field, we can enable the chunks at more or less the right moment.
+ */
+struct recv_item {
+    int fd; // the file descriptor this structure applies to
+    //int mode; // the type of data provider used; not used
+    const struct recv_buffer_t *buf; // Provided recv_buffer_t structure
+    unsigned int chunk_id; // Current chunk, or next chunk to be received
+    size_t bytes_read; // Number of bytes read inside the current chunk
+    struct timespec last_time; // Time of the end of the last call of recv on this socket
+    int64_t interval; // In real-time mode (RECV_REAL_INTERVAL), real wait interval for the current chunk.
+};
+
+struct recv_fd_table_t {
+    size_t n;
+    struct recv_item *items;
+} recv_fd_table;
+
+struct recv_item *recv_get_entry(int fd)
+{
+    for (unsigned int i = 0; i < recv_fd_table.n; i++) {
+        if (recv_fd_table.items[i].fd == fd)
+            return &(recv_fd_table.items[i]);
+    }
+    return NULL;
+}
+
+bool fd_is_recv_buffered(int fd)
+{
+    return (recv_get_entry(fd) != NULL);
+}
+
+void reinit_recv_fd_table_item(int i)
+{
+    // We need to clean up _recv_fd_table.items[i]
+    recv_fd_table.items[i].buf = NULL; // We're not responsible to free it.
+    recv_fd_table.items[i].chunk_id = 0;
+    recv_fd_table.items[i].interval = 0;
+    recv_fd_table.items[i].last_time = (struct timespec) {
+        .tv_sec = 0,
+        .tv_nsec = 0
+    };
+}
+
+void reinit_recv_fd_table()
+{
+    // As we're not responsible to clean up all the recv_buffer_t, we can just free everything up
+    free(recv_fd_table.items);
+    recv_fd_table.n = 0;
+}
+
+int recv_remove_entry(int fd)
+{
+    bool found = false;
+    for (unsigned int i = 0; i < recv_fd_table.n; i++) {
+        if (recv_fd_table.items[i].fd == fd) {
+            found = true;
+            if (i < recv_fd_table.n - 1) {
+                struct recv_item *dest = &(recv_fd_table.items[i]);
+                memmove(dest, dest + 1, recv_fd_table.n - i - 1);
+                memset(&(recv_fd_table.items[recv_fd_table.n-1]), 0, sizeof(struct recv_item));
+                // This is not really a problem that we have a useless thing at the end
+            }
+            recv_fd_table.n--;
+        }
+    }
+    return (found ? 1 : 0);
+}
+
+/**
+ * Returns a pointer to an entry in the table,
+ * where we can safely store our informations.
+ * May be a pre-existing chunk,
+ * in which case the fd's will match.
+ */
+struct recv_item *recv_get_new_entry(int fd)
+{
+    unsigned int i = 0;
+    for (i = 0; i < recv_fd_table.n; i++) {
+        if (recv_fd_table.items[i].fd == fd) {
+            // We should clean it before changing it
+            reinit_recv_fd_table_item(i);
+            break;
+        }
+    }
+    if (i >= recv_fd_table.n) {
+        // realloc
+        struct recv_item *tmp = realloc(recv_fd_table.items, (recv_fd_table.n + 1) * sizeof(struct recv_item));
+        if (tmp == NULL)
+            return NULL;
+        recv_fd_table.items = tmp;
+        recv_fd_table.n++;
+    }
+    // Now, we can insert at index i
+    return &(recv_fd_table.items[i]);
+}
+
+size_t recv_handle_buffer_no_rt(struct recv_item *cur, void *buf, size_t len, int flags, int64_t call_interval)
+{
+    const struct recv_bufchunk_t *curchunk = &(cur->buf->chunks[cur->chunk_id]);
+    if (cur->bytes_read == 0) {
+        // We may have to wait
+        int64_t sleeptime = 0;
+        if (cur->buf->mode == RECV_BEFORE_INTERVAL) {
+            sleeptime = curchunk->interval;
+        } else {
+            sleeptime = curchunk->interval - call_interval;
+        }
+        if (sleeptime > 0) {
+            if ((flags & MSG_DONTWAIT) != 0) {
+                errno = EAGAIN;
+                return -1;
+            }
+            struct timespec tmp = (struct timespec) {
+                .tv_sec = sleeptime / BILLION,
+                .tv_nsec = sleeptime % BILLION
+            };
+            nanosleep(&tmp, NULL);
+            getnanotime(&(cur->last_time)); // We need to update
+        }
+    }
+    size_t bytes_left = curchunk->buflen - cur->bytes_read;
+    size_t transfered_bytes = MIN(len, bytes_left);
+    memmove(buf, (curchunk->buf + cur->bytes_read), transfered_bytes);
+    cur->bytes_read += transfered_bytes;
+    if (cur->bytes_read >= curchunk->buflen) {
+        cur->chunk_id++;
+        cur->bytes_read = 0;
+    }
+    return transfered_bytes;
+}
+
+size_t recv_handle_buffer_rt(struct recv_item *cur, void *buf, size_t len, int flags, int64_t call_interval)
+{
+    /*
+     * Assume the following:
+     * - cur->interval was the time to wait for the current chunk to become active, at the end of the previous call.
+     * - cur->bytes_read is 0 only if we have to wait.
+     */
+    cur->interval -= call_interval;
+    /*
+     * Assume the following:
+     * - cur->interval is the remaining time to wait for the current chunk to become active.
+     * - cur->bytes_read is 0 only if we have to wait.
+     */
+    if (cur->interval > 0) {
+        if ((flags & MSG_DONTWAIT) != 0) {
+            // The call would block but the caller requested it shouldn't block
+            errno = EAGAIN;
+            return -1;
+        }
+        int64_t sleeptime = cur->interval;
+        struct timespec tmp = (struct timespec) {
+            .tv_sec = sleeptime / BILLION,
+            .tv_nsec = sleeptime % BILLION
+        };
+        nanosleep(&tmp, NULL);
+        cur->interval = 0;
+        getnanotime(&(cur->last_time));
+    }
+    /*
+     * Assume the following:
+     * - cur->interval is the negative of the time the current chunk has been available.
+     */
+    size_t total_bytes = 0;
+    for (unsigned int i = cur->chunk_id; i < cur->buf->nchunks; i++) {
+        if (len <= 0) {
+            break;
+        }
+        const struct recv_bufchunk_t *curchunk = &(cur->buf->chunks[i]);
+        size_t bytes_left = curchunk->buflen - cur->bytes_read;
+        size_t transfered_bytes = MIN(len, bytes_left);
+        memmove((buf + total_bytes), (curchunk->buf + cur->bytes_read), transfered_bytes);
+        cur->bytes_read += transfered_bytes;
+        len -= transfered_bytes;
+        total_bytes += transfered_bytes;
+        if (cur->bytes_read >= curchunk->buflen) {
+            // Emptied chunk
+            cur->chunk_id++;
+            cur->bytes_read = 0;
+            if (cur->chunk_id < cur->buf->nchunks) {
+                // Update interval
+                cur->interval += cur->buf->chunks[cur->chunk_id].interval;
+                if (cur->interval > 0) {
+                    break; // Not available yet
+                }
+            }
+        }
+    }
+    return total_bytes;
+}
+
+ssize_t recv_handle_buffer(int sockfd, void *buf, size_t len, int flags)
+{
+    struct recv_item *cur = recv_get_entry(sockfd);
+    if (cur == NULL) {
+        errno = EINTR; // This is about the only case where this can happen
+        return -1;
+    }
+    struct timespec curtime;
+    getnanotime(&curtime);
+    int64_t call_interval = get_time_interval(&(cur->last_time), &curtime);
+    cur->last_time = curtime;
+    if (cur->chunk_id >= cur->buf->nchunks) {
+        // Nothing left to read; we can return immediately 0
+        // TODO remove this as a particular case
+        return 0;
+    }
+    size_t bytes_transfered = 0;
+    if (cur->buf->mode == RECV_BEFORE_INTERVAL || cur->buf->mode == RECV_AFTER_INTERVAL) {
+        bytes_transfered = recv_handle_buffer_no_rt(cur, buf, len, flags, call_interval);
+    } else if (cur->buf->mode == RECV_REAL_INTERVAL) {
+        bytes_transfered = recv_handle_buffer_rt(cur, buf, len, flags, call_interval);
+    } else {
+        return -1;
+    }
+    return bytes_transfered;
+}
+
+/**
+ * Wrap functions.
+ */
 
 
 /*
@@ -211,7 +474,11 @@ ssize_t __wrap_recv(int sockfd, void *buf, size_t len, int flags)
     }
     failures.recv = NEXT(failures.recv);
     ssize_t ret = -1;
-    ret = __real_recv(sockfd, buf, len, flags);
+    if (fd_is_recv_buffered(sockfd)) {
+        ret = recv_handle_buffer(sockfd, buf, len, flags);
+    } else {
+        ret = __real_recv(sockfd, buf, len, flags);
+    }
     return (stats.recv.last_return = ret);
 }
 
@@ -430,6 +697,8 @@ int __wrap_socket(int domain, int type, int protocol)
     return (stats.socket.last_return = ret);
 }
 
+// Additionnal functions
+
 void reinit_network_socket_stats()
 {
     memset(&(stats.accept), 0, sizeof(struct stats_accept_t));
@@ -448,5 +717,38 @@ void reinit_network_socket_stats()
     memset(&(stats.send_all), 0, sizeof(struct stats_send_all_t));
     memset(&(stats.shutdown), 0, sizeof(stats.shutdown));
     memset(&(stats.socket), 0, sizeof(struct stats_socket_t));
+    reinit_recv_fd_table();
+}
+
+int set_recv_data(int fd, const struct recv_buffer_t *buf)
+{
+    if (buf == NULL) {
+        return recv_remove_entry(fd);
+    }
+    if (buf->mode != RECV_REAL_INTERVAL &&
+        buf->mode != RECV_AFTER_INTERVAL &&
+        buf->mode != RECV_BEFORE_INTERVAL) {
+        return -2;
+    }
+    bool already_there = false;
+    if (fd_is_recv_buffered(fd)) {
+        already_there = true;
+    }
+    struct recv_item *tmp = recv_get_new_entry(fd);
+    if (tmp == NULL) {
+        return -1;
+    }
+    tmp->fd = fd;
+    tmp->buf = buf;
+    tmp->chunk_id = 0;
+    tmp->bytes_read = 0;
+    getnanotime(&(tmp->last_time));
+    if (buf->mode == RECV_REAL_INTERVAL && buf->nchunks > 0) {
+        // The first wait interval should be that of the first chunk.
+        tmp->interval = buf->chunks[0].interval;
+    } else {
+        tmp->interval = 0;
+    }
+    return (already_there ? 1 : 0);
 }
 
